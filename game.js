67e62571e71ad5caf02ecfa937ea77dfe959ac48
FrantzLacarriere <frantz.lacarriere@gmail.com>
2014-10-25 17:05:47 -0400

    Added a tick function to reload the Canvas

--- game.js
+++ game.js
@@ -1,21 +1,28 @@
 ;(function() {
   var Game = function(canvasId) {
     var canvas = document.getElementById(canvasId);
+    //draws to the canvas
     var screen = canvas.getContext('2d');
+    //stores the width and height of the canvas
     var gameSize = { x: canvas.width, y: canvas.height };
 
     var self = this;
+    //function the runs all the main game logic
     var tick = function() {
       self.update();
       self.draw(screen, gameSize);
       requestAnimationFrame(tick);
     };
+
+    tick();
   };
 
   Game.prototype = {
-    update: function () {
+    
 
-    }
+  };
 
+  window.onload = function() {
+    new Game("screen");
   };
 })();
