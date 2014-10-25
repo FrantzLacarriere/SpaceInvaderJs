01b9ac7c492881147a9933dc1e4da688a66bd69c
FrantzLacarriere <frantz.lacarriere@gmail.com>
2014-10-24 21:22:41 -0400

    added a basic index with html code and game.js file with a few lines dealing with the Game function constructor and prototype

---
+++ game.js
@@ -0,0 +1,21 @@
+;(function() {
+  var Game = function(canvasId) {
+    var canvas = document.getElementById(canvasId);
+    var screen = canvas.getContext('2d');
+    var gameSize = { x: canvas.width, y: canvas.height };
+
+    var self = this;
+    var tick = function() {
+      self.update();
+      self.draw(screen, gameSize);
+      requestAnimationFrame(tick);
+    };
+  };
+
+  Game.prototype = {
+    update: function () {
+
+    }
+
+  };
+})();
